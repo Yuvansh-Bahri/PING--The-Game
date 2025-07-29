39aa0a85b9a4fb8b994e3270ec9c3bee206d5f7b
DinoNuggetLord <yuvanshbahri@gmail.com>
2025-07-29 00:36:01 -0400

    Add files via upload

--- ping.py
+++ ping.py
@@ -3,8 +3,14 @@
 import pygame, sys
 import random
 
+pygame.mixer.pre_init(44100, -16, 2, 512)
 pygame.init()
+
 clock = pygame.time.Clock() #Event tick basically
+
+# set custom game icon
+image = pygame.image.load('PingLogo.png')
+pygame.display.set_icon(image)
 
 #show stuff
 screen_width = 800
@@ -18,19 +24,37 @@ ball_speed_y = 7 * random.choice((1, -1))
 player_speed = 0
 opponent_speed = 9
 
+# Text lvl 1 vars
+player_pong_score = 0
+opponent_pong_score = 0
+game_font = pygame.font.Font('freesansbold.ttf', 32)
+score_text_colour = pygame.Color('dodgerblue3')
+
+#Score lvl 1 timer
+score_time = True
+
 #BALL-ANIM function
 def ball_anim():
 
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, player_pong_score, opponent_pong_score, score_time
     ball.x += ball_speed_x
     ball.y += ball_speed_y
 
     if ball.top <= 0 or ball.bottom >= screen_height:
         ball_speed_y *= -1
-    if ball.left <= 0 or ball.right >= screen_width:
-        ball_restart()
+
+    if ball.left <= 0: 
+        player_pong_score += 1
+        score_time = pygame.time.get_ticks()
+        pygame.mixer.Sound.play(ping_lvl_1_sound)
+
+    if ball.right >= screen_width:
+        opponent_pong_score += 1
+        score_time = pygame.time.get_ticks()
+        pygame.mixer.Sound.play(ping_lvl_1_sound)
 
     if ball.colliderect(player) or ball.colliderect(opponent):
+        pygame.mixer.Sound.play(ping_lvl_1_sound)
         ball_speed_x *= -1
 
 # Game Rectangles
@@ -45,12 +69,35 @@ opp_colour = pygame.Color('dodgerblue4')
 ball_colour = pygame.Color('dodgerblue4')
 line_colour = pygame.Color('dodgerblue4')
 
+# Sound
+ping_lvl_1_sound = pygame.mixer.Sound('PingPing.mp3')
+score_lvl_1_sound = pygame.mixer.Sound('pong.ogg')
+
 #Defines Ball-Restart func
 def ball_restart():
-    global ball_speed_x, ball_speed_y
+    global ball_speed_x, ball_speed_y, score_time
+
+    current_time = pygame.time.get_ticks()
     ball.center = (screen_width/2, screen_height/2)
-    ball_speed_y *= random.choice((1, -1))
-    ball_speed_x *= random.choice((1, -1))
+
+    if current_time - score_time < 700:
+        number_three = game_font.render('3', False, line_colour)
+        screen.blit(number_three, (screen_width/2 -10, screen_height/2 + 20))
+
+    if 700 < current_time - score_time < 1400:
+        number_two = game_font.render('2', False, line_colour)
+        screen.blit(number_two, (screen_width/2 -10, screen_height/2 + 20))
+
+    if 1400 < current_time - score_time < 2100:
+        number_one = game_font.render('1', False, line_colour)
+        screen.blit(number_one, (screen_width/2 -10, screen_height/2 + 20))
+
+    if current_time - score_time < 2100:
+        ball_speed_x, ball_speed_y = 0, 0
+    else:    
+        ball_speed_y = 7 * random.choice((1, -1))
+        ball_speed_x = 7 * random.choice((1, -1))
+        score_time = None
 
 # Defines player-collision func
 def player_collision():
@@ -102,6 +149,17 @@ while True:
     pygame.draw.rect(screen, opp_colour, opponent)
     pygame.draw.ellipse(screen, ball_colour, ball)
     pygame.draw.aaline(screen, line_colour, (screen_width/2, 0), (screen_width/2, screen_height))
+
+    if score_time:
+        ball_restart()
+
+    #player pong score.vis
+    player_text = game_font.render(f"{player_pong_score}", False, score_text_colour)
+    screen.blit(player_text, (430, 350))
+    #opponent pong score.vis
+    opponent_text = game_font.render(f"{opponent_pong_score}", False, score_text_colour)
+    screen.blit(opponent_text, (350, 350))
+
 
     #Update window
     pygame.display.flip()
